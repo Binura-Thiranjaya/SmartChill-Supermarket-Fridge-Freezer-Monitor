e36b8e54490f007f1b6fd11f0cd2abeb53ff1a95
Binura-Thiranjaya <bt.madoluwage@gmail.com>
2025-05-29 23:32:56 +0100

    remove unwanted files

--- env.js
+++
@@ -1,5 +0,0 @@
-function getSecretKey() {
-    //const AUTH = '8AkVwxIz4ze6f1bl03qCJ31rbci_FCiY';
-    return '8AkVwxIz4ze6f1bl03qCJ31rbci_FCiY';
-    //return "https://blynk.cloud/external/api/get?token=${AUTH}&${pin}"
-}
