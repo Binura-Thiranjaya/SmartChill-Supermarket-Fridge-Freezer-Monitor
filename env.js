209ec5f32de625ef93d801e4bef0e645dc19468c
Binura-Thiranjaya <bt.madoluwage@gmail.com>
2025-05-29 19:04:25 +0100

    Comment Console

---
+++ env.js
@@ -0,0 +1,5 @@
+function getSecretKey() {
+    //const AUTH = '8AkVwxIz4ze6f1bl03qCJ31rbci_FCiY';
+    return '8AkVwxIz4ze6f1bl03qCJ31rbci_FCiY';
+    //return "https://blynk.cloud/external/api/get?token=${AUTH}&${pin}"
+}
